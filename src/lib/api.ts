daa6f0b8c03dfb6dc02d53483098b0656c7de6ff
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-10-05 17:17:16 +0300

    modify fetch function to append bearer token every request

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,10 @@ const baseUrl = "";
 const defaultOptions: RequestInit = { credentials: "include" };
 
 export async function fetchItem<T>(url: string, options?: RequestInit): Promise<T> {
-  const response = await fetch(`${baseUrl}${url}`, { ...options, ...defaultOptions });
+  const headers = new Headers(options?.headers);
+  headers.append("Authorization", `Bearer ${localStorage.getItem("token")}`);
+
+  const response = await fetch(`${baseUrl}${url}`, { ...options, ...defaultOptions, headers });
 
   if (!response.ok) {
     throw new Error(`Network response was not ok. Response status: ${response.status}`);
