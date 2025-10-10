4cbcb411891c6a5d0ea225abdfc8ab910f2304d1
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-10-10 19:33:21 +0300

    add method for verify and login

--- src/modules/Auth/logic/authMethods.ts
+++ src/modules/Auth/logic/authMethods.ts
@@ -14,4 +14,11 @@ export const register = (email: string, password: string) =>
     headers: { "Content-Type": "application/json" }
   });
 
+export const verifyAndLogin = (email: string, password: string, code: string) =>
+  fetchItem("/verify-and-login", {
+    method: "POST",
+    body: JSON.stringify({ email, password, code }),
+    headers: { "Content-Type": "application/json" }
+  });
+
 export const logout = () => fetchItem("/token/blacklist", { method: "POST" });
