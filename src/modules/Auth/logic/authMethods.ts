eac8c46c3fe7a03bf53a8a6c7957d93be4f45caa
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-10-22 12:57:19 +0300

    bring part of the url to a const

--- src/modules/Auth/logic/authMethods.ts
+++ src/modules/Auth/logic/authMethods.ts
@@ -10,27 +10,30 @@ type LoginType = StatusType & {
   };
 };
 
+const API_PART = "/auth";
+
 export const register = (username: string, email: string, password: string) =>
-  fetchItem<RegisterType>("/auth/register", {
+  fetchItem<RegisterType>(`${API_PART}/register`, {
     method: "POST",
     body: JSON.stringify({ username, email, password }),
     headers: { "Content-Type": "application/json" }
   });
 
 export const verifyEmail = (code: string, token: string) =>
-  fetchItem<VerifyType>("/auth/confirm", {
+  fetchItem<VerifyType>(`${API_PART}/confirm`, {
     method: "POST",
     body: JSON.stringify({ code, token }),
     headers: { "Content-Type": "application/json" }
   });
 
 export const login = (email: string, password: string) =>
-  fetchItem<LoginType>("/auth/login", {
+  fetchItem<LoginType>(`${API_PART}/login`, {
     method: "POST",
     body: JSON.stringify({ email, password }),
     headers: { "Content-Type": "application/json" }
   });
 
-export const logout = () => fetchItem<VerifyType>("/auth/logout", { method: "POST" });
+export const logout = () => fetchItem<VerifyType>(`${API_PART}/logout`, { method: "POST" });
 
-export const testLogin = () => fetchItem<LoginType>("/auth/test-login", { method: "POST" });
+export const testLogin = () =>
+  fetchItem<LoginType>(`${API_PART}/test-login`, { method: "POST", credentials: "include" });
